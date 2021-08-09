35a34d58a9cae15836d9c1696063c0f01a452252
student1609 <88677198+student1609@users.noreply.github.com>
2021-08-09 22:22:32 +0530

    Add files via upload

---
+++ info.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+function ageValidation() {
+    var x = document.getElementById("txtAge").value;
+    if (x < 18 || x > 60) {
+        alert("enter age between 18 to 60")
+    }
+}
+
